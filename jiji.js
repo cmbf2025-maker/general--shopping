c2c6cf59edf6ff97b4c90ca2f53f0e5ad3a432e1
cmbf2025-maker <cmbf2025@gmail.com>
2026-06-08 20:14:20 +0100

    Update jiji.js

--- jiji.js
+++ jiji.js
@@ -1,12 +1,20 @@
-
+function run_buy(el){
+  el.setAttribute("setattr","true");
+let priceEl= document.querySelector("span.qa-advert-price-view-value");
+let price = priceEl.getAttribute("content");
+  let nameEl = document.querySelector("div.qa-advert-title");
+  let name = nameEl.innerText;
+  let imageEl = document.querySelector("img.b-slider-image");
+  let image = imageEl.src;
+  let biz_name = typeof BIZ_NAME == "undefined" ? "": BIZ_NAME;
+  location.href = `https://v0-scriptbank.vercel.app/buyProduct?price=${price}&name=${name}&site=jiji.ng&currency=NGN&biz_name=${biz_name}&image=${image}`
+  
+}
 setInterval(function(){
 let ads_button = document.querySelector("div.b-advert-contact-buttons");
 if(ads_button && ! ads_button.getAttribute("setattr")){
 ads_button.addEventListener("click", function(){
-ads_button.setAttribute("setattr","true");
-let priceEl= document.querySelector("span.qa-advert-price-view-value");
-let price = priceEl.getAttribute("content");
-  location.href = `https://v0-scriptbank.vercel.app/buyProduct?price=${price}`
+ run_buy(add_button);
 });
 }
 
@@ -15,10 +23,7 @@ let ads_button2 = document.querySelector("div.b-advert-card-wrapper__bottom");
 
 if(ads_button2 && !ads_button2.getAttribute("setattr")){
 ads_button2.addEventListener("click", function(){
-ads_button2.setAttribute("setattr","true")
-let priceEl= document.querySelector("span.qa-advert-price-view-value");
-let price = priceEl.getAttribute("content");
-  location.href = `https://v0-scriptbank.vercel.app/buyProduct?price=${price}`;      
+ run_buy(ads_button2);
 });
 }
 
