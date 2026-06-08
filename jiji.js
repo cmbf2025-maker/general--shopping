f63259f8d2e9ad90344e07a4aee5dcc9824b7996
cmbf2025-maker <cmbf2025@gmail.com>
2026-06-08 18:38:50 +0100

    Update jiji.js

--- jiji.js
+++ jiji.js
@@ -1,20 +1,24 @@
 let ads_button = document.querySelector("div.b-advert-contact-buttons");
+if(ads_button){
 ads_button.addEventListener("click", function(){
 
 let priceEl= document.querySelector("span.qa-advert-price-view-value");
 let price = priceEl.getAttribute("content");
   location.href = `https://v0-scriptbank.vercel.app/buyProduct?price=${price}`
 });
-
+}
 
 
 let ads_button2 = document.querySelector("div.b-advert-card-wrapper__bottom");
+
+if(ads_button2){
 ads_button2.addEventListener("click", function(){
 
 let priceEl= document.querySelector("span.qa-advert-price-view-value");
 let price = priceEl.getAttribute("content");
   location.href = `https://v0-scriptbank.vercel.app/buyProduct?price=${price}`;      
 });
+}
 
 let similar = document.querySelector("div.h-dflex.h-pb-20.h-flex-dir-column");
 
