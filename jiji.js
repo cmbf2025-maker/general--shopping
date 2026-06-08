63c2654cd97ea419bc7172dc5b0486e2453b3f6a
cmbf2025-maker <cmbf2025@gmail.com>
2026-06-08 14:31:04 +0100

    Update jiji.js

--- jiji.js
+++ jiji.js
@@ -1,8 +1,3 @@
 //base jiji to scriptbank scripts
 
-  if (location.search.includes('debug=true')) {
-    var s = document.createElement('script');
-    s.src = 'https://cdn.jsdelivr.net/npm/eruda';
-    document.body.appendChild(s);
-    s.onload = function() { eruda.init(); };
-  }
+
