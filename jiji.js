dd18b1f93cff9842c37cbb8b690307c61a32d638
cmbf2025-maker <cmbf2025@gmail.com>
2026-06-10 08:11:06 +0100

    Update jiji.js sh share 2

--- jiji.js
+++ jiji.js
@@ -511,7 +511,7 @@ document.head.appendChild( style );
     if (e.changedTouches[0].clientY - startY > 80) closeShare();
   });
     */
-    alert("checks done v3");
+    alert("checks done v4");
   });
   share.setAttribute("shared", "true");
 share_button.parentElement.insertBefore(share, share_button);
